050cc5db2ba83d3b9f5362fca99c23952f9f6815
vains-Sofia <17683906991@163.com>
2025-11-26 12:49:32 +0800

    feat：移除多余路由

--- src/router/modules/static/default.ts
+++ src/router/modules/static/default.ts
@@ -5,22 +5,22 @@ export default [
 		meta: { title: '登录', icon: 'ep:house' },
 		component: () => import('@/views/login/Index.vue'),
 	},
-	{
-		path: '/tools',
-		name: 'tools',
-		meta: { title: '工具集', icon: 'ep:house' },
-		children: [
-			{
-				path: 'video',
-				name: 'video',
-				meta: {
-					title: '视频提取',
-					icon: 'ep:house'
-				},
-				component: () => import('@/views/tools/ExtractVideo.vue'),
-			},
-		],
-	},
+	// {
+	// 	path: '/tools',
+	// 	name: 'tools',
+	// 	meta: { title: '工具集', icon: 'ep:house' },
+	// 	children: [
+	// 		{
+	// 			path: 'video',
+	// 			name: 'video',
+	// 			meta: {
+	// 				title: '视频提取',
+	// 				icon: 'ep:house'
+	// 			},
+	// 			component: () => import('@/views/tools/ExtractVideo.vue'),
+	// 		},
+	// 	],
+	// },
 	{
 		path: '/',
 		name: 'Home',
@@ -36,48 +36,48 @@ export default [
 			},
 		],
 	},
-	{
-		path: '/',
-		name: 'Error',
-		meta: { title: '错误页', icon: 'ep:folder-delete', showLink: true },
-		component: () => import('@/components/Layout/index.vue'),
-		children: [
-			{
-				path: '404',
-				name: '404',
-				component: () => import('@/views/error/404.vue'),
-				meta: { title: '404', icon: 'ep:circle-close', showLink: true },
-			},
-		],
-	},
-	{
-		path: '/aaa',
-		name: 'aaa',
-		meta: { title: '测试', icon: 'ep:house', showLink: true },
-		component: () => import('@/components/Layout/index.vue'),
-		children: [
-			{
-				path: 'level-1',
-				name: 'level-1',
-				component: () => import('@/views/components/Levels.vue'),
-				meta: { title: 'level-1', icon: 'ep:house', showLink: true, showParent: true },
-				children: [
-					{
-						path: 'level-2',
-						name: 'level-2',
-						component: () => import('@/views/components/Levels2.vue'),
-						meta: { title: 'level-2', icon: 'ep:house', showLink: true, showParent: true },
-						children: [
-							{
-								path: 'level-3',
-								name: 'level-3',
-								component: () => import('@/views/components/Levels3.vue'),
-								meta: { title: 'level-3', icon: 'ep:house', showParent: true, activePath: '/aaa/level-1/level-2' },
-							},
-						],
-					},
-				],
-			},
-		],
-	},
+	// {
+	// 	path: '/',
+	// 	name: 'Error',
+	// 	meta: { title: '错误页', icon: 'ep:folder-delete', showLink: true },
+	// 	component: () => import('@/components/Layout/index.vue'),
+	// 	children: [
+	// 		{
+	// 			path: '404',
+	// 			name: '404',
+	// 			component: () => import('@/views/error/404.vue'),
+	// 			meta: { title: '404', icon: 'ep:circle-close', showLink: true },
+	// 		},
+	// 	],
+	// },
+	// {
+	// 	path: '/aaa',
+	// 	name: 'aaa',
+	// 	meta: { title: '测试', icon: 'ep:house', showLink: true },
+	// 	component: () => import('@/components/Layout/index.vue'),
+	// 	children: [
+	// 		{
+	// 			path: 'level-1',
+	// 			name: 'level-1',
+	// 			component: () => import('@/views/components/Levels.vue'),
+	// 			meta: { title: 'level-1', icon: 'ep:house', showLink: true, showParent: true },
+	// 			children: [
+	// 				{
+	// 					path: 'level-2',
+	// 					name: 'level-2',
+	// 					component: () => import('@/views/components/Levels2.vue'),
+	// 					meta: { title: 'level-2', icon: 'ep:house', showLink: true, showParent: true },
+	// 					children: [
+	// 						{
+	// 							path: 'level-3',
+	// 							name: 'level-3',
+	// 							component: () => import('@/views/components/Levels3.vue'),
+	// 							meta: { title: 'level-3', icon: 'ep:house', showParent: true, activePath: '/aaa/level-1/level-2' },
+	// 						},
+	// 					],
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// },
 ]
